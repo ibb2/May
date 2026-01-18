6fcfcf41215bcd396282b16c8b0d0719b07b89df
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-01-19 10:32:33 +1100

    Calendars are now selectedable.

--- app/setup.tsx
+++ app/setup.tsx
@@ -1,28 +1,68 @@
 import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import * as Calendar from "expo-calendar";
+import { Card, Checkbox, cn } from "heroui-native";
 
 export default function Setup() {
-  const [calendars, setCalendars] = useState();
+  const [calendars, setCalendars] = useState<Calendar.Calendar[]>([]);
+  const [selectedCalendars, setSelectedCalendars] = useState<
+    Calendar.Calendar[]
+  >([]);
+
+  function selectCalendar(calendar: Calendar.Calendar) {
+    if (selectedCalendars.includes(calendar) && selectedCalendars.length > 0) {
+      setSelectedCalendars([]);
+
+      const selectedCalendarPos = selectedCalendars.findIndex((c) => {
+        return c.id === calendar.id;
+      });
+
+      const calendarArrayWithoutSelectedCalendar = selectedCalendars.toSpliced(
+        selectedCalendarPos,
+        1,
+      );
+
+      setSelectedCalendars(calendarArrayWithoutSelectedCalendar);
+    } else {
+      setSelectedCalendars([...selectedCalendars, calendar]);
+    }
+  }
 
   useEffect(() => {
     (async () => {
       const { status } = await Calendar.requestCalendarPermissionsAsync();
       if (status === "granted") {
-        const [calendars, events] = await Promise.all([
-          Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT),
-        ]);
-
+        const calendars = await Calendar.getCalendarsAsync(
+          Calendar.EntityTypes.EVENT,
+        );
         setCalendars(calendars);
 
         console.log("Calendars fetched successfully");
+        console.log(calendars.length);
+        // console.log(calendars);
       }
     })();
   }, []);
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Select calendars</Text>
+    <View className="flex-1 items-center justify-center gap-8">
+      <Text className="text-2xl font-bold">Select your calendars</Text>
+      <View className="items-center w-full gap-2">
+        {calendars.map((calendar, index) => (
+          <Text
+            key={calendar.id + index}
+            className={cn(
+              "text-lg",
+              selectedCalendars.includes(calendar)
+                ? "underline"
+                : "no-underline",
+            )}
+            onPress={() => selectCalendar(calendar)}
+          >
+            {calendar.title}
+          </Text>
+        ))}
+      </View>
     </View>
   );
 }
