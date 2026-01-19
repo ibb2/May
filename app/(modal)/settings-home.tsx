22d6de1dae658d1663e8b990ca43cd95a5fd297f
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-01-19 18:13:07 +1100

    Updated settings-home ui.

--- app/(modal)/settings-home.tsx
+++ app/(modal)/settings-home.tsx
@@ -8,30 +8,23 @@ export default function SettingsHomeScreen() {
   const router = useRouter();
 
   return (
-    <Surface className="flex-1 p-4 rounded-none">
-      <View className="flex-1 px-4 py-2">
-        <View className="flex gap-2">
-          <Text className="text-sm text-muted">General</Text>
-          <Surface className="flex gap-2" variant="secondary">
-            <Pressable
-              onPress={() => router.push("/(modal)/settings-calendar")}
-            >
-              <View>
-                <Text>Calendars</Text>
-                <Divider></Divider>
-              </View>
-            </Pressable>
-            <Text>Calendars</Text>
-            <Divider></Divider>
-            <Text>Calendars</Text>
-            <Divider></Divider>
-            <Text>Calendars</Text>
-            <Divider></Divider>
-            <Text>Calendars</Text>
-            <Divider></Divider>
-            <Text>Calendars</Text>
-          </Surface>
-        </View>
+    <Surface className="flex-1 rounded-none">
+      <View className="flex gap-2">
+        <Text className="text-sm text-muted pl-4">General</Text>
+        <Surface className="flex gap-2" variant="secondary">
+          <Pressable onPress={() => router.push("/(modal)/settings-calendar")}>
+            <View className="flex flex-row gap-4">
+              <SymbolView
+                name="calendar"
+                type="monochrome"
+                tintColor="black"
+                size={24}
+              />
+              <Text className="text-lg">Calendars</Text>
+              <Divider></Divider>
+            </View>
+          </Pressable>
+        </Surface>
       </View>
     </Surface>
   );
